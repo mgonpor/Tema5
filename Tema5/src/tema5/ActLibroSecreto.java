a3e7e4727f0394c80d6dea1e582889b9644f2501
miguelGonzalez <mgonpor116@g.educaand.es>
2024-12-10 10:50:25 +0100

    practicar

--- Tema5/src/tema5/ActLibroSecreto.java
+++ Tema5/src/tema5/ActLibroSecreto.java
@@ -1,9 +1,14 @@
 package tema5;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class ActLibroSecreto {
+	
+	public static void main(String[] args) {
+		ejer14();
+	}
 
 	static void ejer07() {
 		Scanner sc = new Scanner(System.in);
@@ -52,7 +57,84 @@ public class ActLibroSecreto {
 			}
 		}
 		System.out.println("]");
-		
+		sc.close();
+	}
+	
+	//Ayuda
+	static boolean esPrimo(int n) {
+		boolean result = true;
+		for(int i = 2; i <= Math.sqrt(n) && result; i++) {
+			if(n%i == 0) {
+				result = false;
+			}
+		}
+		return result;
+	}
+	
+	static void muestraArrayConIndicies(int array[]) {
+		for(int i=0; i<10; i++) {
+			if(array[i]/10>0) {
+				System.out.printf(" %d  ", i);
+			}
+			else {
+				System.out.printf(" %d ", i);
+			}
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(array));
+	}
+	static void ejer11() {
+		Scanner sc = new Scanner(System.in);
+		int array[] = new int[10];
+		System.out.println("Mete 10 números: ");
+		for(int i=0; i<10; i++) {
+			array[i] = sc.nextInt();
+		}
+		//Muestra
+		muestraArrayConIndicies(array);
+		//Cambia
+		int arrayNuevo[] = Arrays.copyOf(array, array.length);
+		int ij=0;
+		int j=array.length-1;
+		for(int elem : arrayNuevo) {
+			if(esPrimo(elem)) {
+				array[ij] = elem;
+				ij++;
+			}else {
+				array[j] = elem;
+				j--;
+			}
+		}
+		//Muestra
+		muestraArrayConIndicies(array);
+		sc.close();
+	}
+	
+	static void ejer14(){
+		String arrayColores[] = {"verde", "rojo", "azul", "amarillo", "naranja", "rosa", "negro", "blanco", "morado"};
+		Arrays.sort(arrayColores);
+		Scanner sc = new Scanner(System.in);
+		String array[] = new String[8];
+		System.out.println("Introduce palabras: ");
+		for(int i=0; i<array.length; i++) {
+			array[i] = sc.next();
+		}
+		sc.close();
+		String arrayNuevo[] = Arrays.copyOf(array, array.length);
+		int i=0;
+		int j=array.length-1;
+		for(String elem : arrayNuevo) {
+			if(Arrays.binarySearch(arrayColores, elem) > -1) {
+				array[i] = elem;
+				i++;
+			}
+			else {
+				array[j] = elem;
+				j--;
+			}
+		}
+		//Muestra
+		System.out.println(Arrays.toString(array));
 	}
 	
 }
